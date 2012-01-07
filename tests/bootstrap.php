3324c535415b92a6a816e6eb46b2385ecf681339
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-01-07 18:22:30 +0100

    Removing symfony class loader, let composer handle autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
 require __DIR__.'/../vendor/.composer/autoload.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$classLoader = new UniversalClassLoader;
-
-$classLoader->registerNamespace('Pipe', array(
-    realpath(__DIR__.'/../lib'),
-    realpath(__DIR__)
-));
-
-$classLoader->register();
