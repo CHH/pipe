9ee87fe75e3b7aba49f7f7e085811b9d92d277f3
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-05-15 16:43:44 +0200

    Added initial standalone asset server powered by React.

--- lib/Pipe/BundledAsset.php
+++ lib/Pipe/BundledAsset.php
@@ -4,13 +4,6 @@ namespace Pipe;
 
 class BundledAsset extends Asset
 {
-    function getProcessors()
-    {
-        $parentProcessors = parent::getProcessors();
-
-        return array_merge($parentProcessors, $bundleProcessors);
-    }
-
     function getBody()
     {
         if (null === $this->body) {
