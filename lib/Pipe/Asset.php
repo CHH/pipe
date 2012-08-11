6081fc4e69e3fa124dfb5ce9521c63f4cbdca4cc
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-08-11 10:13:34 +0200

    Added extension handling back to base asset, Asset::write() now takes an array of options.

--- lib/Pipe/Asset.php
+++ lib/Pipe/Asset.php
@@ -2,6 +2,8 @@
 
 namespace Pipe;
 
+use Pipe\Util\Pathname;
+
 abstract class Asset
 {
     public $path;
@@ -9,6 +11,7 @@ abstract class Asset
 
     # The asset's declared dependencies.
     public $dependencies = array();
+    public $digest;
 
     protected $environment;
     protected $body;
@@ -74,14 +77,24 @@ abstract class Asset
         return dirname($this->path);
     }
 
-    function getChecksum()
+    function getDigest()
     {
-        return sha1($this->getBody());
+        if (null === $this->digest) {
+            $this->digest = sha1($this->getBody());
+        }
+
+        return $this->digest;
     }
 
     function getLastModified()
     {
         return filemtime($this->path);
+    }
+
+    function getDigestName()
+    {
+        $ext = $this->getFormatExtension();
+        return preg_replace('/(\.\w+)$/', "-{$this->getDigest()}$ext", $this->logicalPath);
     }
 
     # Public: Returns the filename with extension, that's appropiate
@@ -107,17 +120,129 @@ abstract class Asset
 
     # Public: Writes the asset's content to the directory.
     #
-    # directory  - Directory to write the asset to, optional.
+    # options - Array of options.
+    #           dir:            Write the asset to the given directory. (default: '')
+    #           include_digest: Should the digest be spliced into the filenames? (default: false)
+    #           compress:       Should the contents be GZIP compressed? (default: false)
     #
     # Returns Nothing.
-    function write($directory = '')
+    function write($options = array())
     {
-        $filename = ($directory ? "$directory/" : '') . $this->getTargetName();
+        $dir = @$options["dir"];
+        $compress = @$options["compress"] ?: false;
+        $includeDigest = @$options["include_digest"] ?: false;
+
+        $filename = Pathname::join(array($dir, ($includeDigest ? $this->getDigestName() : $this->logicalPath)));
 
         if (!is_dir(dirname($filename))) {
             mkdir(dirname($filename), 0777, true);
         }
 
-        @file_put_contents($filename, $this->getBody());
+        if ($compress) {
+            $body = gzencode($this->getBody(), 9);
+            $filename .= ".gz";
+        } else {
+            $body = $this->getBody();
+        }
+
+        @file_put_contents($filename, $body);
+    }
+
+    # Determines the format extension.
+    #
+    # The format extension is the extension which is not
+    # assigned to an engine and is present in the environment's
+    # configured content types.
+    #
+    # Example:
+    #
+    #   $asset = $env->find("/foo.js");
+    #   echo $asset->getFormatExtension();
+    #   // .js
+    #
+    #   $asset = $env->find("/foo.coffee.js");
+    #   echo $asset->getFormatExtension();
+    #   // .js
+    #
+    #   $asset = $env->find("/foo.coffee");
+    #   echo $asset->getFormatExtension();
+    #   // .js
+    #
+    # Returns a String.
+    function getFormatExtension()
+    {
+        $environment = $this->environment;
+
+        $formatExtension = current(array_filter(
+            $this->getExtensions(),
+            function($ext) use ($environment) {
+                return
+                    isset($environment->contentTypes[$ext])
+                    and !$environment->engines->get($ext);
+            }
+        ));
+
+        return $formatExtension
+            ?: array_search($this->getEngineContentType(), $environment->contentTypes)
+            ?: join('', $this->getExtensions());
+    }
+
+    # Collects the file's extensions.
+    #
+    # Returns an Array of normalized extensions.
+    protected function getExtensions()
+    {
+        if (null === $this->extensions) {
+            $basename = $this->getBasename();
+
+            # Avoid treating name of a dotfile as extension by
+            # ignoring dots at the first offset in the string
+            if (!$basename or false === ($pos = strpos($basename, '.', 1))) {
+                return array();
+            }
+
+            $extensions = explode('.', substr($basename, $pos + 1));
+
+            $this->extensions = array_map(function($ext) {
+                return Pathname::normalizeExtension($ext);
+            }, $extensions);
+        }
+
+        return $this->extensions;
+    }
+
+    protected function getEngineContentType()
+    {
+        foreach ($this->getEngineExtensions() as $ext) {
+            $engine = $this->environment->engines->get($ext);
+
+            if (is_callable(array($engine, "getDefaultContentType"))) {
+                return $engine::getDefaultContentType();
+            }
+        }
+    }
+
+    protected function getEngines()
+    {
+        $env = $this->environment;
+
+        return array_map(
+            function($ext) use ($env) {
+                return $env->engines->get($ext);
+            },
+            $this->getEngineExtensions()
+        );
+    }
+
+    protected function getEngineExtensions()
+    {
+        $environment = $this->environment;
+
+        return array_filter(
+            $this->getExtensions(),
+            function($ext) use ($environment) {
+                return $environment->engines->get($ext);
+            }
+        );
     }
 }
