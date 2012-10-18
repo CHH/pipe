c4f345a1e4c89948bccbbd3f0bf74259b7e31855
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-10-18 11:52:04 +0200

    Fixed exception class name, disable process timeout

--- lib/Pipe/Compressor/BaseYuglifyCompressor.php
+++ lib/Pipe/Compressor/BaseYuglifyCompressor.php
@@ -21,10 +21,13 @@ abstract class BaseYuglifyCompressor extends \MetaTemplate\Template\Base
         $process = $builder->getProcess();
         $process->setStdin($data);
 
+        # Deactivate process timeout, otherwise minifying large projects can time out.
+        $process->setTimeout(null);
+
         $process->run();
 
         if (!$process->isSuccessful()) {
-            throw new \UnexpectedArgumentException(sprintf(
+            throw new \UnexpectedValueException(sprintf(
                 'Error while compressing "%s": %s', $this->source, $process->getErrorOutput()
             ));
         }
